7db316b9178c96c63452981ce30bcd7d0dfecda1
KevinDeBroyer <33628645+KevinDeBroyer@users.noreply.github.com>
2021-03-26 15:31:28 +0100

    Menu Change
    Working on menu

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const navSlide = () => {
-    const burger = document.querySelector( '.burger' );
-    const nav = document.querySelector( '.nav-links' );
-    const navlinks = document.querySelectorAll('.navlinks li')
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('.nav-active');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  
+    navLinks.forEach((link, index) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        burger.classList.toggle('toggle');
+        nav.classList.toggle('nav-active');
+        navLinks.forEach(setStyleLink)
+      })
     });
-}
-navSlide();
+  
+    burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+      // animate navlinks
+      navLinks.forEach(setStyleLink);
+      // burger animation
+      burger.classList.toggle('toggle');
+    });
+  }
+  
+  function setStyleLink(el, index) {
+    if (el.style.animation) {
+      el.style.animation = ''
+    } else {
+      el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+    }
+  }
+  navSlide();
